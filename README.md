3b58c7f27d4a074427a9c201185f086cb02a30ec
Robpiot <robin.piot@outlook.com>
2023-09-14 11:35:21 +0200

    added the slides to the READMe.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tech-talks-css-library
+
+
+
+
+slides : https://docs.google.com/presentation/d/1EZiCM1IgqE2PM6yM83NAOxvz4pL0ov1h-Dnkab5xk4Q/edit?usp=sharing
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge : {
+    preserveHtmlElements : false,
+    content: ["./index.html"],
+  },
+  theme: {
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
